41f530a3ac6697be4be50f6a46b5fe408e3ba5da
nickmarietta <nick.marietta@csu.fullerton.edu>
2024-11-20 11:38:53 -0800

    added boilerplate and main files

---
+++ Problem1/prob1.py
@@ -0,0 +1,9 @@
+# this is the code for problem 1, solve using priority queue
+# input: nums = [1,1,1,2,2,3], k = 2
+# output: [1,2]
+
+import heapq
+from collections import Counter
+
+def topMostKFrequent(nums, k):
+
