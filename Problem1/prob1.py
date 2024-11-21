b46c59693407dcf3852f9393b2114613a232efcc
nickmarietta <nick.marietta@csu.fullerton.edu>
2024-11-20 21:04:09 -0800

    formatted and finished both problems

--- Problem1/prob1.py
+++ Problem1/prob1.py
@@ -3,7 +3,42 @@
 # output: [1,2]
 
 import heapq
-from collections import Counter
 
-def topMostKFrequent(nums, k):
 
+def topMostKFrequent(nums: list[int], k: int) -> list[int]:
+    # create a map to store the frequency of each element
+    map = dict()
+
+    # iterate through the list and store the frequency of each element
+    for num in nums:
+        map[num] = map.get(num, 0) + 1
+
+    # create a heap
+    heap = []
+
+    # now iterate through map and store the frequency in the heap
+    for key, value in map.items():
+        heapq.heappush(heap, (value, key))
+        # if the size of the heap is greater than k, pop the smallest element
+        if len(heap) > k:
+            heapq.heappop(heap)
+
+    # take the keys from the heap and then return them
+    return [key for value, key in heap]
+
+
+# testing
+nums = [1, 1, 1, 2, 2, 3]
+k = 2
+print("Example 1: nums = [1, 1, 1, 2, 2, 3], k = 2  Output:", topMostKFrequent(nums, k))
+
+nums = [1]
+k = 1
+print("Example 2: nums = [1], k = 1  Output:", topMostKFrequent(nums, k))
+
+nums = [1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 5, 5, 5]
+k = 3
+print(
+    "Example 3: nums = [1,1,1,2,2,3,4,4,4,4,5,5,5], k = 3  Output:",
+    topMostKFrequent(nums, k),
+)
--- Problem2/prob2.py
+++ Problem2/prob2.py
@@ -1,37 +1,45 @@
 from collections import defaultdict
 import heapq
 
+
 def network(times, n, k):
-    #times = source node, destination node, time taken to travel
-    #n = number of nodes
-    #k = starting node
+    # times = source node, destination node, time taken to travel
+    # n = number of nodes
+    # k = starting node
     heap = []
 
     heapq.heappush(heap, [0, k])
 
     graph = defaultdict(list)
-    #puts all nodes in an adjacency list
+    # puts all nodes in an adjacency list
     for u, v, w in times:
         graph[u].append((v, w))
-    #creates a set to keep track of visited nodes
+    # creates a set to keep track of visited nodes
     visited = set()
-    #while the heap is not empty
+    # while the heap is not empty
     while heap:
-        #get current node and time from heap
+        # get current node and time from heap
         current_node_time, node = heapq.heappop(heap)
-        #if node is not in visited, add it to visited
+        # if node is not in visited, add it to visited
         if node not in visited:
             visited.add(node)
-            #if all nodes are visited return time
+            # if all nodes are visited return time
             if len(visited) == n:
                 return current_node_time
-            #check neighbors of current node and add their time to the heap
+            # check neighbors of current node and add their time to the heap
             for neighbor, neighbor_time in graph[node]:
                 heapq.heappush(heap, [current_node_time + neighbor_time, neighbor])
-    #return -1 if not all nodes can be reached
+    # return -1 if not all nodes can be reached
     return -1
 
-print("Example 1 [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2  Output: ", network([[2,1,1],[2,3,1],[3,4,1]], 4, 2))
-print("Example 2 [[1,2,1]], n = 2, k = 1  Output: ", network([[1,2,1]], 2, 1))
-print("Example 3 [[1,2,1]], n = 2, k = 2  Output: ", network([[1,2,1]], 2, 2))
-print("Example 4 [[1,3,1],[1,2,4],[4,2,1],[3,4,1]], n = 4, k = 1  Output: ", network([[1,3,1],[1,2,4],[4,2,1],[3,4,1]], 4, 1))
+
+print(
+    "Example 1 [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2  Output: ",
+    network([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2),
+)
+print("Example 2 [[1,2,1]], n = 2, k = 1  Output: ", network([[1, 2, 1]], 2, 1))
+print("Example 3 [[1,2,1]], n = 2, k = 2  Output: ", network([[1, 2, 1]], 2, 2))
+print(
+    "Example 4 [[1,3,1],[1,2,4],[4,2,1],[3,4,1]], n = 4, k = 1  Output: ",
+    network([[1, 3, 1], [1, 2, 4], [4, 2, 1], [3, 4, 1]], 4, 1),
+)
